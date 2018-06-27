70144d534841299aadf941bea65078a144d9dbb3
Dan Marshall <danmar@microsoft.com>
2018-06-26 23:12:12 -0700

    react component

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,39 @@
 /// <reference types="deckgl-typings" />
 
 import { DeckGL, OrbitView, PolygonLayer, COORDINATE_SYSTEM } from 'deck.gl';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-new DeckGL({
-    parent: document.body,
-    views: [
-        new OrbitView()
-    ],
-    viewState: {
-        distance: 50,
-        rotationX: 64,
-        rotationOrbit: 72,
-        fov: 50,
-        zoom: 0.2
-    },
-    layers: [
-        new PolygonLayer({
-            coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
-            data: [
-                {
-                    polygon: [[0, 0], [100, 0], [0, 100]],
-                    elevation: 50,
-                    fillColor: [255, 0, 0]
-                }
-            ],
-            extruded: true,
-            stroked: false
-        })
-    ]
-});
+export class Example extends React.Component {
+
+    render() {
+        return <DeckGL
+            views={[
+                new OrbitView()
+            ]}
+            viewState={{
+                distance: 50,
+                rotationX: 64,
+                rotationOrbit: 72,
+                fov: 50,
+                zoom: 0.2
+            }}
+            layers={[
+                new PolygonLayer({
+                    coordinateSystem: COORDINATE_SYSTEM.IDENTITY,
+                    data: [
+                        {
+                            polygon: [[0, 0], [100, 0], [0, 100]],
+                            elevation: 50,
+                            fillColor: [255, 0, 0]
+                        }
+                    ],
+                    extruded: true,
+                    stroked: false
+                })
+            ]}
+        />;
+    }
+}
+
+ReactDOM.render(React.createElement(Example), document.getElementById('app'));
